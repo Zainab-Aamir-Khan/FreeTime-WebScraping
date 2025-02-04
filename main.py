170e5abdcab748f33c678817f627d99b8caa61db
Zainab Aamir <zaamir427@gmail.com>
2025-02-04 22:44:14 +0500

    ..

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ soup = BeautifulSoup(url, 'lxml')
 print("lets get the Book and Quote detail of a website!!!")
 
 main = soup.find('body')
-print(main.prettify())
+# print(main.prettify())
 
+heading = main.find('h2')
+print(heading.text)
+
+details = main.find('div', class_= 'col-md-6')
+print(details.text)
 
 # bookInfo =  main.find_all('div', class_ ='col-md-10')
 # print(bookInfo)
