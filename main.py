d8dc4eb4e6c850726b6952a313bc9ba97e4f3cc8
Zainab Aamir <zaamir427@gmail.com>
2025-02-04 22:32:05 +0500

    ..

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ import requests
 url = requests.get("https://toscrape.com/").text
 soup = BeautifulSoup(url, 'lxml')
 print(requests.get("https://toscrape.com/").status_code)
+
+print("lets print the Book and Quote detail of a website!!!")
+
