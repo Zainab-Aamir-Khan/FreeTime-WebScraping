0ef90454011a722190195048ed839883ba5bfd30
Zainab Aamir <zaamir427@gmail.com>
2025-02-04 22:35:52 +0500

    ..

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import requests
 
 url = requests.get("https://toscrape.com/").text
 soup = BeautifulSoup(url, 'lxml')
-print(requests.get("https://toscrape.com/").status_code)
+# print(requests.get("https://toscrape.com/").status_code)
 
 print("lets get the Book and Quote detail of a website!!!")
 
+main = soup.find('body').text
+
+bookInfo =  main.find('table', class_ ='table table-hover')
+print(bookInfo)
+
+
+''
